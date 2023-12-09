1073436e2f0ea3bcc8577e2b7a1eaa26667738d5
LakshayTrehan77 <133641907+LakshayTrehan77@users.noreply.github.com>
2023-12-09 23:24:23 +0530

    Delete PROJECT SAMPLE directory

--- PROJECT SAMPLE/src/main/java/com/example/projectsample/Level.java
+++
@@ -1,6 +0,0 @@
-package com.example.projectsample;
-
-// Interface for game levels
-interface Level {
-    void loadLevel();
-}
--- PROJECT SAMPLE/src/main/java/com/example/projectsample/StickHeroGameExeption.java
+++
@@ -1,9 +0,0 @@
-package com.example.projectsample;
-
-abstract class StickHeroGameException extends Exception {
-    public StickHeroGameException(String message) {
-        super(message);
-    }
-
-    // You can add additional methods or fields common to all exceptions
-}
